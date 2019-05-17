cc68b3621aefea12e169eba9e8afc572a2911c2a
CliffCrerar <cliff.crerar@gmail.com>
2019-05-17 21:46:26 +0200

    Update

--- run.js
+++ run.js
@@ -7,8 +7,9 @@ const path = require('path');
 //const url = 'https://nodejs.org/en/'
 //const url = 'https://materializecss.com/'
 //const url = 'https://portal.azure.com/#@cliffcrerargmail.onmicrosoft.com/dashboard/private/e361c483-2557-4033-b373-3315e527a00f'
-const url="https://login.microsoftonline.com/"
-const appName = 'Azure-Cloud'
+//const url="https://login.microsoftonline.com/"
+const url="https://github.com/CliffCrerar?tab=repositories"
+const appName = 'Github.com'
 const savePath = path.join(__dirname, "nativefied", appName);
 
 
