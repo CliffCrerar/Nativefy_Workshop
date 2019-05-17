d3adf14ceaa2b4194a8149426622698141a9e44f
CliffCrerar <cliff.crerar@gmail.com>
2019-05-18 00:48:19 +0200

    Yes

--- run.js
+++ run.js
@@ -8,8 +8,9 @@ const path = require('path');
 //const url = 'https://materializecss.com/'
 //const url = 'https://portal.azure.com/#@cliffcrerargmail.onmicrosoft.com/dashboard/private/e361c483-2557-4033-b373-3315e527a00f'
 //const url="https://login.microsoftonline.com/"
-const url="https://github.com/CliffCrerar?tab=repositories"
-const appName = 'Github.com'
+//const url="https://github.com/CliffCrerar?tab=repositories"
+const url = 'https://nextjs.org/docs'
+const appName = 'Next.js-Docs'
 const savePath = path.join(__dirname, "nativefied", appName);
 
 
